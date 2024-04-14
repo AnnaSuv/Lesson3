3818b744e47fec9d5a43629658677d29ad47e204
Anna Suv <multguga@gmail.com>
2024-04-14 17:12:58 +0300

    Lesson_3add

---
+++ Task1/Program.cs
@@ -0,0 +1,12 @@
+﻿/* Вычислите значение формулы a*b/(c+d), где а, b,
+с и d -некоторые целые числа. Решение оформить в виде функции.
+*/
+
+double CalculatedFormula(int a, int b, int c, int d);
+{
+    int numerator = a * b;
+    int denominator = c + d;
+    double result = numerator / denominator;
+    return result;
+}
+CalculatedFormula(17, 13, 8, 2);
